079079fd9b60d6c5fb80143d48d374c974a899e9
Nikola Misic <nikola.misic@farfetch.com>
2022-02-09 15:39:05 +0000

    Creating account number

---
+++ BankOcrTests/AccountNumberTests.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * constructor que aceita string
+ *
+ * Property IsValid
+ *
+ * 
+ */
+
+namespace BankOcrTests
+{
+    using Xunit;
+
+    public class AccountNumberTests
+    {
+        [Fact]
+        public void CreateInstance()
+        {
+            new AccountNumber("");
+        }
+    }
+
+    public class AccountNumber
+    {
+        public AccountNumber(string accountNumber)
+        {
+            
+        }
+    }
+}
