8dc8227f6c5019d5b86d7e6624a64a0974df87ac
Nikola Misic <nikola.misic@farfetch.com>
2022-02-09 15:45:38 +0000

    Moving things around

---
+++ BankOcr.Logic/AccountNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace BankOcr.Logic
+{
+    public class AccountNumber
+    {
+        private readonly string accountNumber;
+        
+        public AccountNumber(string accountNumber)
+        {
+            this.accountNumber = accountNumber;
+        }
+
+        public bool IsValid => new ChecksumCalculator().Calculate(this.accountNumber) % 11 == 0;
+    }
+}
--- BankOcrTests/AccountNumberTests.cs
+++ BankOcrTests/AccountNumberTests.cs
@@ -8,22 +8,25 @@
 
 namespace BankOcrTests
 {
+    using BankOcr.Logic;
     using Xunit;
 
     public class AccountNumberTests
     {
         [Fact]
-        public void CreateInstance()
+        public void IsValid_ValidAccount()
         {
-            new AccountNumber("");
-        }
-    }
-
-    public class AccountNumber
-    {
-        public AccountNumber(string accountNumber)
-        {
+            var account = new AccountNumber("457508000");
             
+            Assert.True(account.IsValid, "457508000 is not valid account number");
+        }
+
+        [Fact]
+        public void IsValid_InvalidAccountNumber()
+        {
+            var account = new AccountNumber("664371495");
+            
+            Assert.False(account.IsValid, "664371495 is valid account number");
         }
     }
 }
