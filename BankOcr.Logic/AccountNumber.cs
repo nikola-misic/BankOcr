6d81d46d1a08ff85f43a6f1239318d9cfd1efa6e
Nikola Misic <nikola.misic@farfetch.com>
2022-02-09 16:06:47 +0000

    Implementing account state

--- BankOcr.Logic/AccountNumber.cs
+++ BankOcr.Logic/AccountNumber.cs
@@ -1,14 +1,43 @@
 ﻿namespace BankOcr.Logic
 {
+    public enum AccountNumberState
+    {
+        Valid,
+        Error,
+        Illegible
+    }
+
     public class AccountNumber
     {
         private readonly string accountNumber;
+        private AccountNumberState state;
         
         public AccountNumber(string accountNumber)
         {
             this.accountNumber = accountNumber;
+
+            if (this.accountNumber.Contains("?"))
+            {
+                this.state = AccountNumberState.Illegible;
+            }
+            else
+            {
+                if (IsValid)
+                {
+                    this.state = AccountNumberState.Valid;
+                }
+                else
+                {
+                    this.state = AccountNumberState.Error;
+                }
+            }
         }
 
         public bool IsValid => new ChecksumCalculator().Calculate(this.accountNumber) % 11 == 0;
+
+        public AccountNumberState GetStatus()
+        {
+            return state;
+        }
     }
 }
--- BankOcrTests/AccountNumberTests.cs
+++ BankOcrTests/AccountNumberTests.cs
@@ -3,7 +3,10 @@
  *
  * Property IsValid
  *
- * 
+ * GetStatus
+ * - valid number
+ * - invalid number
+ * - illegible number
  */
 
 namespace BankOcrTests
@@ -20,13 +23,37 @@ namespace BankOcrTests
             
             Assert.True(account.IsValid, "457508000 is not valid account number");
         }
-
+        
         [Fact]
         public void IsValid_InvalidAccountNumber()
         {
             var account = new AccountNumber("664371495");
             
-            Assert.False(account.IsValid, "664371495 is valid account number");
+            Assert.False(account.IsValid, "664371495 is not valid account number");
+        }
+
+        [Fact]
+        public void GetStatus_ValidAccountNumber()
+        {
+            var actual = new AccountNumber("457508000").GetStatus();
+            
+            Assert.Equal(AccountNumberState.Valid, actual);
+        }
+        
+        [Fact]
+        public void GetStatus_InvalidAccountNumber()
+        {
+            var actual = new AccountNumber("664371495").GetStatus();
+            
+            Assert.Equal(AccountNumberState.Error, actual);
+        }
+        
+        [Fact]
+        public void GetStatus_IllegibleAccountNumber()
+        {
+            var actual = new AccountNumber("86110??36").GetStatus();
+            
+            Assert.Equal(AccountNumberState.Illegible, actual);
         }
     }
 }
