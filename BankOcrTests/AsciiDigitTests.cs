61222be6c1a2a567370138f5936e532192d9833a
Nikola Misic <nikola.misic@farfetch.com>
2022-02-09 11:56:53 +0000

    Advancing with Equals implementation

--- BankOcrTests/AsciiDigitTests.cs
+++ BankOcrTests/AsciiDigitTests.cs
@@ -35,6 +35,42 @@ namespace BankOcrTests
             
             Assert.Equal(expected, actual);
         }
+        
+        [Fact]
+        public void Equals_ThreeEqualsThree()
+        {
+            var expected = new AsciiDigit(new char[3,3] {
+                {' ', '_' ,' '},
+                {' ', '_', '|'},
+                {' ', '_', '|'}
+            });
+            
+            var actual = new AsciiDigit(new char[3,3] {
+                {' ', '_' ,' '},
+                {' ', '_', '|'},
+                {' ', '_', '|'}
+            });
+            
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        public void Equals_OneNotEqualsTwo()
+        {
+            var expected = new AsciiDigit(new char[3,3] {
+                {' ', ' ' ,' '},
+                {' ', ' ', '|'},
+                {' ', ' ', '|'}
+            });
+            
+            var actual = new AsciiDigit(new char[3,3] {
+                {' ', '_' ,' '},
+                {' ', '_', '|'},
+                {'|', '_', ' '}
+            });
+            
+            Assert.NotEqual(expected, actual);
+        }
     }
 
     public class AsciiDigit
@@ -48,14 +84,30 @@ namespace BankOcrTests
 
         public override string ToString()
         {
-            return $"{matrix[0, 0]}{matrix[0, 1]}{matrix[0, 2]}\n" +
+            return $"\n{matrix[0, 0]}{matrix[0, 1]}{matrix[0, 2]}\n" +
                    $"{matrix[1, 0]}{matrix[1, 1]}{matrix[1, 2]}\n" +
                    $"{matrix[2, 0]}{matrix[2, 1]}{matrix[2, 2]}\n";
         }
 
         public override bool Equals(object obj)
         {
-            return true;
+            if (obj is AsciiDigit other)
+            {
+                for (int i = 0; i < matrix.Rank + 1; i++)
+                {
+                    for (int j = 0; j < (matrix.Length / (matrix.Rank + 1)); j++)
+                    {
+                        if (this.matrix[i, j] != other.matrix[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
