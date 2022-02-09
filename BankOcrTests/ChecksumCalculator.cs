5938c2ea9b7abecad399d91bc049e40259d3e19a
Nikola Misic <nikola.misic@farfetch.com>
2022-02-09 15:36:20 +0000

    Adding missing changes

---
+++ BankOcrTests/ChecksumCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace BankOcrTests
+{
+    public class ChecksumCalculator
+    {
+        public int Calculate(string accountNumber)
+        {
+            int checksum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                checksum += (9 - i) * int.Parse(accountNumber[i].ToString());
+            }
+
+            return checksum;
+        }
+    }
+}
--- BankOcrTests/ChecksumCalculatorTests.cs
+++ BankOcrTests/ChecksumCalculatorTests.cs
@@ -41,18 +41,4 @@ namespace BankOcrTests
             Assert.Equal(expected, actual);
         }
     }
-
-    public class ChecksumCalculator
-    {
-        public int Calculate(string accountNumber)
-        {
-            int checksum = 0;
-            for (int i = 0; i < 9; i++)
-            {
-                checksum += (9 - i) * int.Parse(accountNumber[i].ToString());
-            }
-
-            return checksum;
-        }
-    }
 }
